0f607809083d78e3417991e4d47f9d78e5ad880b
Eztrela <pablolm.estrela@gmail.com>
2023-03-28 22:19:27 -0300

    adicionando integração de estacionamento com a interface

--- TesteEstacionamento.java
+++ TesteEstacionamento.java
@@ -3,14 +3,14 @@ public class TesteEstacionamento {
 
 	public static void main(String[] args) {
 		try {
-			Estacionamento est = new Estacionamento(5);
-			est.entrar("MMY9325", 1);
-			est.entrar("MMY9324", 2);
-			est.sair(2);
-			est.sair(1);
-			est.entrar("MMY9324", 2);
-			est.gravarDados();
-			System.out.println(est.listarGeral());
+//			Estacionamento est = new Estacionamento(5);
+//			est.entrar("MMY9325", 1);
+//			est.entrar("MMY9324", 2);
+//			est.sair(2);
+//			est.sair(1);
+//			est.entrar("MMY9324", 2);
+//			est.gravarDados();
+			InterfaceValetinho interfaceGrafica = new InterfaceValetinho();
 		} catch (Exception e) {
 			System.out.println(e.getMessage());
 		}
